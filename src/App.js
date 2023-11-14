89f13ecf342e0146c3d65e773d03f16da86b8a97
Derek Mackey <mackey838@gmail.com>
2023-11-14 16:58:16 -0500

    finish skeleton code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex min-h-screen min-w-screen justify-center">
+      <div className="bg-white rounded-[22px] min-h-full flex flex-col basis-3/4 my-[3%] p-10">
+        <div>Exit</div>
+        <div className="flex justify-center">Document Upload</div>
+        <div className="flex flex-row">
+          <div className="basis-3/5">
+            <div className="flex flex-col">
+              <div className="text-skyhop-blue font-bold">
+                Select import name:
+              </div>
+              <div>Line</div>
+              <div className="text-skyhop-blue font-bold">
+                Select a manifest you'd like to import:
+              </div>
+              <div>Line</div>
+              <div className="text-skyhop-blue font-bold">
+                Elapse Data Checking:
+              </div>
+              <div className="text-success font-bold">No Elapsed Dates!</div>
+              <div>Line</div>
+              <div className="text-skyhop-blue font-bold">
+                Tolerance Window:
+              </div>
+            </div>
+          </div>
+          <div className="basis-2/5">
+            <div className="flex flex-col">
+              <div className="text-skyhop-blue font-bold">
+                Split Schedule using social distancing?
+              </div>
+              <div>Line</div>
+              <div className="text-skyhop-blue font-bold">
+                Location Checking:
+              </div>
+              <div className="text-success font-bold">All Available!</div>
+              <div>Line</div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          Data in the import file is correct. Please press continue to import.
+        </div>
+        <div className="flex flex-row justify-center">
+          <button className="p-4">Continue Import</button>
+          <button className="p-4">Cancel</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "skyhop-blue": "#1c3e6e",
+      "skyhop-orange": "#f99d26",
+      success: "#4eb66b",
+      white: "#ffffff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
